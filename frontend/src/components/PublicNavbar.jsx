cdcc39b7fafe4d2954de68779296c5ae71560883
Kenny <kennykotten@gmail.com>
2025-02-08 15:23:27 -0800

    add nav button borders

--- frontend/src/components/PublicNavbar.jsx
+++ frontend/src/components/PublicNavbar.jsx
@@ -5,11 +5,26 @@ const PublicNavbar = () => {
     <nav className="p-4 bg-slate-100">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex gap-4">
-          <Link to="/">Home</Link>
-          <Link to="/page1">Public Page 1</Link>
-          <Link to="/page2">Public Page 2</Link>
+          <Link to="/" className="border border-gray-400 px-3 py-1 rounded">
+            Home
+          </Link>
+          <Link
+            to="/page1"
+            className="border border-gray-400 px-3 py-1 rounded"
+          >
+            Contact Form Tester/Public Page 1
+          </Link>
+          <Link
+            to="/page2"
+            className="border border-gray-400 px-3 py-1 rounded"
+          >
+            Public Page 2
+          </Link>
         </div>
-        <Link to="/login" className="btn">
+        <Link
+          to="/login"
+          className="border border-gray-500 px-4 py-2 rounded bg-white hover:bg-gray-100 transition"
+        >
           myDChi Portal
         </Link>
       </div>
